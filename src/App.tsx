c72bd4f1200e43d3cba03755f56314a66d5ee42e
Jose Viejo <jviejo@gmail.com>
2021-04-19 00:17:16 +0200

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ import {
 } from "react-router-dom";
 import './index.css'
 import jwt_decode from "jwt-decode";
- 
-import FeaturePage from './feature'
-
 import KeycloakProvider, { configureKeycloak, PrivateRoute, Login, Logout } from 'react-router-keycloak';
+import Hero from './Hero';
+import Faq from './Faq';
+import Feature from './feature';
 
 const KEYCLOAK_URL = 'https://sso.gttinternacional.com';
 const KEYCLOAK_REALM = 'jviejo100';
@@ -21,14 +21,14 @@ const KEYCLOAK_CLIENT_ID = 'sgr-ui';
 configureKeycloak(KEYCLOAK_URL, KEYCLOAK_REALM, KEYCLOAK_CLIENT_ID);
 
 function App() {
-  const [token, setToken] = useState<string|null>(null);
-  
+  const [token, setToken] = useState<any | null>(null);
+
   function handleRefresh(token: any) {
     console.log('Called every time the token is refreshed so you can update it locally', token);
-    var decoded:string = jwt_decode(token);
+    var decoded: string = jwt_decode(token);
     setToken(decoded)
   }
-  
+
   return (
     <KeycloakProvider
       loginPath="/login"
@@ -42,28 +42,29 @@ function App() {
                 <Link to="/">Home</Link>
               </li>
               <li className="p-3">
-                 <Link to="/about">About</Link>
+                <Link to="/about">About</Link>
               </li>
               <li className="p-3">
-                <Link to="/users">Users</Link>
+                <Link to="/faq">faq</Link>
               </li>
-              <li className="p-3">
-                <Link to="/private">privado</Link>
-              </li>
-              <li className="p-3">
-                <Link to="/log-out">salir</Link>
-              </li>
+              {!token &&
+                <li className="p-3">
+                  <Link to="/private">Login</Link>
+                </li>
+              }
+              {token &&
+                <li className="p-3">
+                  <Link to="/log-out">Logout ({token.name.substring(0,3)})</Link>
+                </li>
+              }
             </ul>
           </nav>
-
-          {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-          <div className="p-3">
           <Switch>
-            <Route path="/about"><FeaturePage /></Route>
-            <Route path="/users"><div>users</div></Route>
-            <Route path="/login" render={props => <Login
-              onFailure={(err:any) => {
+
+            <Route path="/about" exact={true} render={props => <Feature></Feature>} />
+            <Route path="/faq" exact={true} render={props => <Faq></Faq>} />
+            <Route path="/login" exact={true} render={props => <Login
+              onFailure={(err: any) => {
                 console.log("error ", err);
 
               }}
@@ -71,17 +72,15 @@ function App() {
                 //console.log("success", token);
                 var decoded: string = jwt_decode(token);
                 setToken(decoded)
-              }} redirectTo="/private" {...props} />} ></Route>
-            <Route path="/log-out"
-
+              }} redirectTo="/private" {...props} />} />
+            <Route path="/log-out" exact={true}
               render={props => <Logout onSuccess={(evt: any) => {
                 console.log("success", evt);
 
               }} redirectTo="/" {...props} />} />
-            <PrivateRoute path="/private" component={() => <pre>{token && JSON.stringify(token,null, 5)}</pre>} />
-            <Route path="/"><div>root </div></Route>
+            <PrivateRoute exact={true} path="/private" component={() => <pre>{token && JSON.stringify(token, null, 5)}</pre>} />
+            <Route path="/" exact={true} render={props => <Hero></Hero>} />
           </Switch>
-          </div>
         </div>
       </Router>
     </KeycloakProvider>
